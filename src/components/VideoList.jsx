f4e112150cd5fac8466e4cce30c8e195c154cdce
mlee0210 <lee.cs.michael@gmail.com>
2018-06-27 17:27:38 -0700

    implement main video player, video list, title, description

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,13 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <h5>
+      {props.videos.map(video =>
+        <VideoListEntry video={video}/>
+      )}
+    </h5>
   </div>
 );
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
