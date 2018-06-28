3e485a144516cf4d09930117070ebe2c80a77c07
mlee0210 <lee.cs.michael@gmail.com>
2018-06-27 22:13:53 -0700

    implement main video player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,52 @@
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer videos={props.videos}/> 
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={props.videos} />
-      </div>
-    </div>
-  </div>
-);
+// var App = (props) => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <Search />
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer /> 
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos={props.videos} />
+//       </div>
+//     </div>
+//   </div>
+// );
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={props.videos[0]} /> 
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.props.videos} />
+          </div>
+        </div>
+      </div>    
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+
